6255906052348cbf74e49dbc58aa71215e528f30
Vlad <vladstavila@gmail.com>
2024-01-17 21:13:06 +0200

    first commit

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useGlobalContext } from "../context";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+
+const ThemeToggle = () => {
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+  return (
+    <section className="toggle-container">
+      <button type="button" className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? <BsFillMoonFill className="toggle-icon" /> : <BsFillSunFill className="toggle-icon" />}
+      </button>
+    </section>
+  );
+};
+
+export default ThemeToggle;
